b1647de9bfe56be33c84c539fd0b7eda55ae99b9
Mehrdad Javidan <31864084+mehrdadjavidan@users.noreply.github.com>
2017-10-22 16:11:32 +0330

    by Javidan

--- app/src/main/java/project/parsdata/com/V/MainActivity.java
+++ app/src/main/java/project/parsdata/com/V/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                             intent.putExtra("ActiveCode", sActiveCode);
                             startActivity(intent);
 
-                            Toast.makeText(MainActivity.this,"کد فعالسازی 4y5 برای شما ارسال شد.",
+                            Toast.makeText(MainActivity.this,"کد فعالسازی 6 برای شما ارسال شد.",
                                     Toast.LENGTH_SHORT).show();
 
                         }
